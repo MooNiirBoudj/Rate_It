3594cb2ecf15a79df8723e9ea59eb10f964267de
eyadmahmoudi <131752017+eyadmahmoudi@users.noreply.github.com>
2025-01-10 12:45:44 +0100

    Update dbhelper.dart

--- lib/DB/dbhelper.dart
+++ lib/DB/dbhelper.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 import 'dart:io';
-//import 'package:flutter/material.dart';
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'db_user_table.dart';
-//import 'db_user_table.dart'; // Import the DBUserTable file
 
 class DBHelper {
   static const _database_name = "Authentification.db";
@@ -29,16 +28,22 @@ class DBHelper {
     _database = await openDatabase(
       join(await getDatabasesPath(), _database_name),
       onCreate: (db, version) {
-        // Loop through all SQL table creation codes
         for (var sql in sql_codes) {
+          print('Executing SQL: $sql');
           db.execute(sql);
         }
       },
       version: _database_version,
-      onUpgrade: (db, oldVersion, newVersion) {
-        // Handle database upgrades here if necessary
-      },
     );
     return _database!;
   }
+
+  Future<void> deleteOldDatabase() async {
+  final dbPath = await getDatabasesPath();
+  final path = join(dbPath, DBHelper._database_name);
+  await deleteDatabase(path);
+  print('Old database deleted: $path');
+}
+
+  
 }
